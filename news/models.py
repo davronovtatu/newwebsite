75a1f7523183bf66f21a66f18c595a1e5e54b7aa
davronovtatu <davronovtatu@gmail.com>
2024-01-27 08:52:54 +0500

    loyiha

---
+++ news/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+from django.contrib.auth.models import User
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=self.model.Status.Published)
+
+
+class Category(models.Model):
+    name=models.CharField(max_length=250)
+
+
+class News(models.Model):
+
+    class Status(models.TextChoices):
+        Draft="DF","Draft"
+        Published="PB","Published"
+
+    title=models.CharField(max_length=250)
+    slug=models.SlugField(max_length=250)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    image=models.ImageField(upload_to="news_image/")
+    body=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    publish_time=models.DateTimeField(default=timezone.now)
+    status=models.CharField(max_length=2,choices=Status.choices,default=Status.Draft)
+
+    objects=models.Manager() #default
+    published=PublishedManager() #ozgartirilgan
+
+
+    class Meta:
+        ordering = ["-publish_time"]
+
+
+class Comment(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    new=models.ForeignKey(News,on_delete=models.CASCADE)
+    body=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+
+
+
+
+
+
+
+
+
+
