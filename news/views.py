81bd2e70c924920f31a2bb3d97198349de4dd94b
davronovtatu <davronovtatu@gmail.com>
2024-01-27 18:01:06 +0500

    ikkinchi ozgarish

--- news/views.py
+++ news/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from .models import News
 # Create your views here.
 def HomeView(request):
-    return render(request,'index.html')
-
+    latest_new=News.published.order_by('-id').first()
+    news=News.published.order_by('-id')[:5]
+    context={
+        "latest_new":latest_new,
+        "news":news,
+    }
+    return render(request,'index.html',context)
